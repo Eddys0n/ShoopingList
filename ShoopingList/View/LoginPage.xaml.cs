c4ad61130f846b51f0e5242ce50cc1a7b44aec20
Edison <mcyfabox@gmail.com>
2025-12-04 00:14:14 -0600

    Added Login, Logout, Create user

--- ShoopingList/View/LoginPage.xaml.cs
+++ ShoopingList/View/LoginPage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using ShoopingList.Models;
 
 namespace ShoopingList.View;
 
@@ -13,10 +15,29 @@ public partial class LoginPage : ContentPage
         InitializeComponent();
     }
 
-    private void Login_OnClicked(object sender, EventArgs e)
+    async void Login_OnClicked(object sender, EventArgs e)
     {
-        App.SessionKey = "HolaMundo";
-        Navigation.PopModalAsync();
+        //User Info
+        //
+        
+        var data = JsonConvert.SerializeObject(new UserAccount(txtUser.Text, txtPass.Text));
+
+        var client = new HttpClient();
+        var  response = await client.PostAsync(new Uri("https://joewetzel.com/fvtc/account/login"),
+            new StringContent(data, Encoding.UTF8, "application/json"));
+        
+        var SKey = response.Content.ReadAsStringAsync().Result;
+
+        if (!string.IsNullOrEmpty(SKey) && SKey.Length < 50 )
+        {
+            App.SessionKey = SKey;
+            Navigation.PopModalAsync();
+        }
+        else
+        {
+            await DisplayAlert("Error", "Sorry Invalid Username or Password", "OK");
+            return;
+        }
     }
 
     private void NewAccount_OnClicked(object sender, EventArgs e)
